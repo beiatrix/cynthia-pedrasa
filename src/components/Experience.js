95f58a905960105e35dcebeca0e90007a75f7d70
Beiatrix Pedrasa <bpedrasa@gmail.com>
2019-04-17 11:34:12 -0400

    continue populating experience data

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -17,6 +17,40 @@ const expData = [
       "Designed CJR algorithms for early identification of lower extremity joint replacement through integration of information from various registrations, emrs, surgical scheduling systems, billing and payment systems across14 Northwell hospitals to help initiate patient interventions prior to admission for better patient outcomes",
       "Managed product design for Population Health Tasking feature and Medication list feature that interfaces with the hospitals health information exchange to integrate medications from inpatient, outpatient/ambulatory, and regional health information systems to assist the Care Navigators in the medication review during the transition of care, complex care management, and long term care to support the value-based programs (Bundled programs, Health Home, Practice-based care management, and Insurance/Managed care)"
     ]
+  },
+  {
+    company: "CenterLight Healthcare (formerly CCM)",
+    role: "Nurse Informatics Manager",
+    date: "June 2009 - July 2012",
+    length: "3 years 2 months",
+    location: "?",
+    details: [
+      "Business Lead for the Medical Management Care Transitions project, which reduced avoidable hospital readmissions by > 2%",
+      "Successfully eliminated backlog in paper-based standardized assessments through process reengineering with 85% improvement in assessment sequencing and 69% reduction in late assessments, improving regulatory compliance over a period of 12 months",
+      "Developed SQL queries/stored procedures to support performance improvement efforts. Automated key performance indicator reports through SQL integration services and analysis services",
+      "Managed a team of Data Entry Operators and Informatics Nurse",
+      "Managed all aspects of software design, testing, training, and implementation of clinical assessments-SAAM & OASIS-C",
+      "Conducted implementation trainings for 200+ nurses and coordinated deployment of laptops for field nurses",
+      "Reengineered clinical work processes, ensured compliance with SAAM & OASIS, and increased operational efficiencies",
+      "Developed SharePoint sites to improve collaboration on projects and operational needs. Created SharePoint sites and custom forms for the PACE, Long Term Managed Care and Licensed Home Care Agency Programs"
+    ]
+  },
+  {
+    company: "Center for Nursing and Rehabilitation",
+    role: "Systems Analyst/Informatics Nurse",
+    date: "May 2002 - June 2009",
+    length: "7 years 2 months",
+    location: "?",
+    details: [
+      "Projects include:",
+      "Occurrences System development",
+      "OCS OASIS, EQUIP benchmarking systems implementation",
+      "Conversion from ADL to ADP/HR Perspective to Lawson",
+      "Implementation of McKesson Horizon Homecare (ADHC, Lombardi, CHHA, Licensed Agencies), custom clinical forms development; forms mapping/ LOINC mapping; evidence-based guidelines/care plans, Telehealth, and Telephony",
+      "X12 EDI 837/835, 270/271 analysis and implementation",
+      "Electronic Billing and Payment Processing of Vendor Services; developed electronic claim forms for commercial insurance",
+      "SigmaCare system selection and implementation"
+    ]
   }
 ]
 
