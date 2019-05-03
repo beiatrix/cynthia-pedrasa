da447893cbc0c169d5dd1563562025097e4fa99c
Beiatrix Pedrasa <bpedrasa@gmail.com>
2019-05-03 16:40:05 -0400

    Updates

---
+++ precache-manifest.8dfe29bed44ca04f8786af2c9b51f461.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "d8ec8ff305558bb2ee4b",
+    "url": "/static/js/main.c7ac9c78.chunk.js"
+  },
+  {
+    "revision": "5f2b36ce41e8e06fad00",
+    "url": "/static/js/2.e2778465.chunk.js"
+  },
+  {
+    "revision": "d8ec8ff305558bb2ee4b",
+    "url": "/static/css/main.7720414a.chunk.css"
+  },
+  {
+    "revision": "804f6d433e6ef608815cbbaf20ee99c4",
+    "url": "/index.html"
+  }
+];
