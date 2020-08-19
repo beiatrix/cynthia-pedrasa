f9694d2ca0685d42d268a314dc96f5c0e7818d02
cynthiapedrasa <cpedrasa@gmail.com>
2020-08-18 23:28:26 -0400

    Updates

--- precache-manifest.9bd36da6c2079ca7dba96ef4c5276aab.js
+++ precache-manifest.9bd36da6c2079ca7dba96ef4c5276aab.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "49d6d11f7261dcdea205",
+    "revision": "35c96b192896a7c472dc",
     "url": "/static/css/main.e1da463b.chunk.css"
   },
   {
-    "revision": "49d6d11f7261dcdea205",
-    "url": "/static/js/main.25b419ea.chunk.js"
+    "revision": "35c96b192896a7c472dc",
+    "url": "/static/js/main.7d5fe8af.chunk.js"
   },
   {
     "revision": "42ac5946195a7306e2a5",
@@ -16,7 +16,7 @@ self.__precacheManifest = [
     "url": "/static/js/2.dcf7e645.chunk.js"
   },
   {
-    "revision": "680270de4c1df2dbf4ea49eae8a2518c",
+    "revision": "886dda2122e43f210d6876bea3b487f2",
     "url": "/index.html"
   }
 ];
